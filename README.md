19247bf8a526f8af7642ae2a535fb36d679a6704
krnbr <krnbr@live.in>
2022-08-22 23:47:41 +0530

    global-infra like cert and dns entry for cert's DNS validation

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - A minimalistic structure will be generated
 
 :warning: typescript is added as part of dev dependencies to make the tsc run better while compiling the lambda function's code.
+
+- starting at the root folder level of project - which is inside `pulumi-aws-lmabda`
+- `mkdir global-infra` && `cd global-infra` - create a folder dedicated to the global infra (like cert and route 53 DNS, public zone was manually created, it is referred)
+- `pulumi new aws-typescript` - init pulumi using aws-typescript template
+- the config for the cert domain(wildcard cert) and the hosted zone domain - are made part of the config
---
+++ global-infra/index.ts
@@ -0,0 +1,33 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const config = new pulumi.Config();
+const cert_domain_name = config.require("cert_domain_name")
+const base_domain_name = config.require("base_domain_name")
+
+const zone = aws.route53.getZoneOutput({
+    name: base_domain_name
+});
+
+const cert: aws.acm.Certificate = new aws.acm.Certificate(cert_domain_name, {
+    domainName: cert_domain_name,
+    validationMethod: 'DNS',
+}, {
+    retainOnDelete: true
+});
+
+cert.domainValidationOptions.apply(opts => {
+    let i = 0;
+    let records: aws.route53.Record[] = [];
+    for (const option of opts) {
+        let record = new aws.route53.Record(`validation-record-${i++}`, {
+            type: option.resourceRecordType,
+            zoneId: zone.zoneId,
+            name: option.resourceRecordName,
+            ttl: 600,
+            records: [option.resourceRecordValue],
+        });
+        pulumi.log.info(`created ${i} record ${JSON.stringify(option)}`);
+        records.push(record);
+    }
+});
