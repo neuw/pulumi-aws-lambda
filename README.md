3ca19d27dbc974d98285363639e927b6a7b73526
krnbr <krnbr@live.in>
2022-08-23 19:42:00 +0530

    added example for public hosted zone also

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 :warning: typescript is added as part of dev dependencies to make the tsc run better while compiling the lambda function's code.
 
 - starting at the root folder level of project - which is inside `pulumi-aws-lmabda`
-- `mkdir global-infra` && `cd global-infra` - create a folder dedicated to the global infra (like cert and route 53 DNS, public zone was manually created, it is referred)
+- `mkdir global-infra` && `cd global-infra` - create a folder dedicated to the global infra (like cert and route 53 DNS, public zone was manually created, it is referred through domain, but we may also generate that programmatically)
 - `pulumi new aws-typescript` - init pulumi using aws-typescript template
 - the config for the cert domain(wildcard cert) and the hosted zone domain - are made part of the config
 
-:warning: For all the sub modules(infra, apis, etc) the AWS_PROFILE env variable was present, and only then the pulumi up command used to work - otherwise there will be a runtime error
+:warning: For all the submodules(infra, apis, etc.) the AWS_PROFILE env variable was present, and only then the pulumi up command used to work - otherwise there will be a runtime error
--- global-infra/index.ts
+++ global-infra/index.ts
@@ -2,35 +2,77 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const config = new pulumi.Config();
-const cert_domain_name = config.require("cert_domain_name")
-const base_domain_name = config.require("base_domain_name")
+const app_cert_domain_name = config.require("app_cert_domain_name")
+const app_base_domain_name = config.require("app_base_domain_name")
+const api_cert_domain_name = config.require("api_cert_domain_name")
+const api_base_domain_name = config.require("api_base_domain_name")
 
-const zone = aws.route53.getZoneOutput({
-    name: base_domain_name
+
+// refer based on existing - was created through console
+const app_zone = aws.route53.getZoneOutput({
+    name: app_base_domain_name
 });
 
-const cert: aws.acm.Certificate = new aws.acm.Certificate(cert_domain_name, {
-    domainName: cert_domain_name,
+// otherwise may create it as below
+const api_zone = new aws.route53.Zone(api_base_domain_name, {
+    name: api_base_domain_name,
+    comment: "dedicated to the api routes"
+});
+
+// aws wildcard cert for apps
+const app_cert: aws.acm.Certificate = new aws.acm.Certificate(app_cert_domain_name, {
+    domainName: app_cert_domain_name,
     validationMethod: 'DNS',
 }, {
     retainOnDelete: true
 });
 
-cert.domainValidationOptions.apply(opts => {
+app_cert.domainValidationOptions.apply(opts => {
     let i = 0;
-    let records: aws.route53.Record[] = [];
+    let app_records: aws.route53.Record[] = [];
     for (const option of opts) {
-        let record = new aws.route53.Record(`validation-record-${i++}`, {
+        i++;
+        let record = new aws.route53.Record(`app-dns-validation-record-${i}`, {
             type: option.resourceRecordType,
-            zoneId: zone.zoneId,
+            zoneId: app_zone.zoneId,
             name: option.resourceRecordName,
             ttl: 600,
             records: [option.resourceRecordValue],
+            allowOverwrite: false
         });
         pulumi.log.info(`created ${i} record ${JSON.stringify(option)}`);
-        records.push(record);
+        app_records.push(record);
     }
 });
 
-export const defaultZoneId = zone.zoneId;
-export const defaultCertArn = cert.arn;
+// aws wildcard cert for apis
+const api_cert: aws.acm.Certificate = new aws.acm.Certificate(api_cert_domain_name, {
+    domainName: api_cert_domain_name,
+    validationMethod: 'DNS',
+}, {
+    retainOnDelete: true
+});
+
+api_cert.domainValidationOptions.apply(opts => {
+    let i = 0;
+    let api_records: aws.route53.Record[] = [];
+    for (const option of opts) {
+        i++;
+        let record = new aws.route53.Record(`api-dns-validation-record-${i}`, {
+            type: option.resourceRecordType,
+            zoneId: api_zone.zoneId,
+            name: option.resourceRecordName,
+            ttl: 600,
+            records: [option.resourceRecordValue],
+            allowOverwrite: false
+        });
+        pulumi.log.info(`created ${i} record ${JSON.stringify(option)}`);
+        api_records.push(record);
+    }
+});
+
+export const appZoneId = app_zone.zoneId;
+export const appCertArn = app_cert.arn;
+export const api_dns_nameservers = api_zone.nameServers; // helpful for configuring this to your domain provider - specially when it is non AWS.
+export const apiZoneId = api_zone.zoneId;
+export const apiCertArn = api_cert.arn;
